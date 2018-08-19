f787bc58f1a57634c849a3600fee8fcf461b7d7e
chullino <chullino93@gmail.com>
2018-08-19 12:01:34 +0900

    last commit

---
+++ src/Components/App/AppContainer.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react"
+import { injectGlobal } from "styled-components"
+import AppPresenter from "./AppPresenter"
+import reset from "styled-reset"
+import axios from "axios"
+import flatten from "lodash.flatten"
+import typography from "../../typography"
+import { API_URL, WS_URL } from "../../constants"
+import { parseMessage } from "../../utils"
+
+const baseStyles = () => injectGlobal`
+    ${reset}
+    ${typography}
+    a{
+        text-decoration:none!important;
+    }
+`
+
+class AppContainer extends Component {
+    state = {
+        isLoading: true
+    }
+
+    componentDidMount = () => {
+        this._getData()
+        this._connectToWs()
+    }
+
+    render() {
+    	baseStyles()
+        return <AppPresenter {...this.state} />
+   }
+
+    _getData = async () => {
+        const request = await axios.get(`${API_URL}/blocks`)
+        const blocks = request.data
+        const reversedBlocks = blocks.reverse()
+        const transactions = flatten(reversedBlocks.map(block => block.data))
+        this.setState({
+            blocks: reversedBlocks,
+            transactions: transactions,
+            isLoading: false
+        })
+    }
+
+    _connectToWs = () => {
+        const ws = new WebSocket(WS_URL)
+        ws.addEventListener("message", message => {
+            const parsedMessage = parseMessage(message)
+            if (parsedMessage !== null && parsedMessage !== undefined) {
+                this.setState(prevState => {
+                    return {
+                        ...prevState,
+                        blocks: [...parsedMessage, ...prevState.blocks],
+                        transactions: [...parsedMessage[0].data, ...prevState.transactions]
+                    }
+                })
+            }
+        })
+    }
+ }
+
+export default AppContainer
---
+++ src/Routes/Blocks/BlocksPresenter.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const BlocksPresenter = () => "blocks";
+
+export default BlocksPresenter;
+
+
